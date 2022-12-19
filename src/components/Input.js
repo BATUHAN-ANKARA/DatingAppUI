d352d14a726209976a114873cba61403f066464e
Batuhan-ankara <sertgumecbatuhan@gmail.com>
2022-12-19 15:16:39 +0300

    19.12 First Commit

--- src/components/Input.js
+++ src/components/Input.js
@@ -4,10 +4,39 @@ import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const Input = ({inputMessage, onSendPress, setMessage}) => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Input Screen</Text>
+    <View style={styles.container}>
+      <TextInput
+        placeholder='Some Text'
+        value={inputMessage}
+        onChangeText={setMessage}
+        style={styles.input}
+      />
+      <TouchableOpacity>
+        <Ionicons name='ios-send' color='#fff' size={20} />
+      </TouchableOpacity>
     </View>
   )
 }
 
 export default Input
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,2)',
+    width: '85%',
+    position: 'absolute',
+    bottom: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 30,
+  },
+  input: {
+    fontSize: 11,
+    paddingHorizontal: 10,
+    flex: 1,
+    color: '#fff ',
+  },
+})
--- src/components/Message.js
+++ src/components/Message.js
@@ -1,12 +1,70 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, TouchableOpacity, StyleSheet, Image} from 'react-native'
+import LinearGradient from 'react-native-linear-gradient'
 
-const Message = () => {
+const randomTime = () => {
+  const hrs = Math.round(Math.random() * 12)
+  const mins = Math.round(Math.random() * 60)
+  const hFormat = hrs < 10 ? '0' : ''
+  const mFormat = mins < 10 ? '0' : ''
+  const amPm = hrs < 12 ? 'AM' : 'PM'
+  return String(hFormat + hrs + ':' + mFormat + mins + ' ' + amPm)
+}
+
+const Message = ({username, uri, count, onPress}) => {
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Message Screen</Text>
-    </View>
+    <TouchableOpacity style={styles.container} onPress={onPress}>
+      {count > 0 ? (
+        <LinearGradient
+          colors={['#f26a50', '#f20045', '#f20045']}
+          style={styles.gradientStyle}>
+          <Text style={styles.count}>{count}</Text>
+        </LinearGradient>
+      ) : null}
+      <Image source={{uri: uri}} style={styles.image} />
+      <View style={{marginLeft: 10}}>
+        <Text style={styles.username}>{username}</Text>
+        <Text style={styles.text}>Hello, How are you</Text>
+      </View>
+      <Text style={styles.duration}>{randomTime}</Text>
+    </TouchableOpacity>
   )
 }
 
 export default Message
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    paddingHorizontal: 20,
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  gradientStyle: {
+    height: 20,
+    width: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginRight: 20,
+  },
+  count: {
+    color: '#fff',
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+  },
+  username: {
+    color: '#b6b6b6',
+    fontSize: 11,
+  },
+  duration: {
+    color: '#000119',
+    fontSize: 12,
+    flex: 1,
+    marginLeft: 280,
+    position: 'absolute',
+  },
+})
